e51b7d9e2bdf34258f0c57833f5972e877318493
ErickEduardo027 <es2022-3938@uce.edu.do>
2025-09-20 02:31:47 -0400

    ADD: DbConnectionFactory,Configuración de appsettings.json y program.cs con arreglo de errores

---
+++ Data2Web.Data/Context/DatabaseContent.cs
@@ -0,0 +1,32 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data2Web.Data.Context
+{
+    public interface IDbConnectionFactory
+    {
+        IDbConnection Create();
+    }
+
+    public sealed class DbConnectionFactory : IDbConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public DbConnectionFactory(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("Default")
+                ?? throw new InvalidOperationException("Falta ConnectionStrings:Default en appsettings.json");
+        }
+
+        public IDbConnection Create()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
